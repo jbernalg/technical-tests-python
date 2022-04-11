43f1ad49df205d82d874cd0f1dd7908fce6d9a55
jbernalg <jeinffersonbernal@gmail.com>
2022-04-11 16:54:46 -0500

    modificacion descripcion counting

--- countingSort1.py
+++ countingSort1.py
@@ -2,8 +2,8 @@
 #Instead, you create an integer array whose index range covers the entire 
 #range of values in your array to sort. Each time a value occurs in 
 #the original array, you increment the counter at that index. 
-#At the end, run through your counting array, 
-#printing the value of each non-zero valued index that number of times.
+#At the end, run through your counting array, printing the value of each 
+# non-zero valued index that number of times.
 
 def countingSort(arr):
     valMax = 100
