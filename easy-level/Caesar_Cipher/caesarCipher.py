3f0c52624a37d8adf3ef0b5f277a1396ad0692d7
Jeinfferson B <99054174+jbernalg@users.noreply.github.com>
2022-04-14 09:28:18 -0500

    Actualizacion caesar_cipher.py

--- easy-level/Caesar_Cipher/caesarCipher.py
+++ easy-level/Caesar_Cipher/caesarCipher.py
@@ -1,16 +1,3 @@
-#Julius Caesar protected his confidential information by encrypting it using a cipher. 
-#Caesar's cipher shifts each letter by a number of letters. If the shift takes you 
-#past the end of the alphabet, just rotate back to the front of the alphabet. 
-#In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
-
-#Example
-#s = there's-a-starman-waiting-in-the-sky
-#k = 3
-
-#The alphabet is rotated by 3, matching the mapping above. The encrypted string is 
-#wkhu'v-d-vwdupdp-zdlwlqj-lq-wkh-vnb.
-
-#Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
 import string
 
 def caesarCipher(s, k):
