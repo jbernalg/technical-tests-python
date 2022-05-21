63458161aed85655393ec6492cf694cdb95dd50a
jbernalg <jeinffersonbernal@gmail.com>
2022-05-20 21:19:57 -0500

    new year chaos modified

--- easy-level/NewYearChaos/newYearChaos.py
+++ easy-level/NewYearChaos/newYearChaos.py
@@ -1,4 +1,5 @@
 def minimunBribes(q):
+    #counter: number of bribes
     counter= 0
     for i in range(len(q)-1,0,-1):
         if q[i]!=i+1:
@@ -10,17 +11,18 @@ def minimunBribes(q):
                 q[i-2],q[i-1]= q[i-1],q[i-2]
                 q[i-1],q[i]= q[i],q[i-1]
             else:
-                print('Too chaotic')
-                return
-    print(counter)
+                return 'Too chaotic'
+                
+    return counter
 
 
 if __name__ == '__main__':
     #proof number
     t = int(input().strip())
 
-    for t_itr in range (t):
+    for t_itr in range(t):
         n = int(input().strip())
         q = list(map(int, input().rstrip().split()))
 
-        minimunBribes(q)
+        result = minimunBribes(q)
+        print(result)
